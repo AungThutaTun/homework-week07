3304da9b1c417a558ee748c97694b8f613ee78a7
thuta98 <tripletthuta@gmail.com>
2025-10-20 02:59:57 +0900

    Initial commit

---
+++ lib/week07/calender_scheduler/database/drift_database.dart
@@ -0,0 +1,2 @@
+import 'package:myapp/week07/calender_scheduler/model/schedule.dart';
+import '';
---
+++ lib/week07/calender_scheduler/model/schedule.dart
@@ -0,0 +1,11 @@
+import 'dart:ffi';
+
+import 'package:drift/drift.dart';
+
+class Schedules extends Table{
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get title => text()();
+  DateTimeColumn get date => dateTime()();
+  IntColumn get startTime => integer()();
+  IntColumn get endTime => integer()();
+}
